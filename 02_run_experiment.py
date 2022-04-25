72d264d997174ca4fbc79f70f22345a34f769b19
Julius Gonsior <julius@gaenseri.ch>
2022-04-25 20:33:13 +0200

    started polishing up 02_run_experiment.py

--- 02_run_experiment.py
+++ 02_run_experiment.py
@@ -6,7 +6,11 @@ import os
 
 config = Config()
 
-print(config.WORKER_INDEX)
+log_it(
+    "Executing Job # {} of workload {}".format(
+        config.WORKER_INDEX, config.WORKLOAD_FILE_PATH
+    )
+)
 
 random_seed_df = pd.read_csv(
     config.WORKLOAD_FILE_PATH,
@@ -14,13 +18,13 @@ random_seed_df = pd.read_csv(
     index_col=0,
     nrows=config.WORKER_INDEX + 1,
 )
-print(random_seed_df)
 worker_dataset_id, worker_strategy_id, worker_random_seed = random_seed_df.loc[config.WORKER_INDEX]  # type: ignore
 
-print(worker_dataset_id)
-print(worker_strategy_id)
-print(worker_random_seed)
-
+log_it(
+    "Job Parameters are dataset_id {}, strategy_id {} and random_seed {}".format(
+        worker_dataset_id, worker_strategy_id, worker_random_seed
+    )
+)
 
 # take a look at the mapping from id to dataset + id to strategy_id
 # then run the experiment!
