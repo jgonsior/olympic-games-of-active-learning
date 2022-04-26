8138ff05236d10814058b2b1590aee7d50885c63
Julius Gonsior <julius@gaenseri.ch>
2022-04-26 18:08:32 +0200

    dowload from kaggle

--- 02_run_experiment.py
+++ 02_run_experiment.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 from misc.config import Config, Strategy
 from misc.logging import log_it
 
@@ -19,19 +19,24 @@ random_seed_df = pd.read_csv(
 )
 worker_dataset_id, worker_strategy_id, worker_random_seed = random_seed_df.loc[config.WORKER_INDEX]  # type: ignore
 
+dataset = Dataset(worker_dataset_id)
+strategy = Strategy(worker_strategy_id)
+
 log_it(
     "Job Parameters are dataset_id {}-{}, strategy_id {}-{} and random_seed {}".format(
         worker_dataset_id,
-        Dataset(worker_dataset_id).name,
+        dataset.name,
         worker_strategy_id,
-        Strategy(worker_strategy_id).name,
+        strategy.name,
         worker_random_seed,
     )
 )
 
 
 # load dataset
-# load_dataset(worker_dataset_id, config)
+df = load_dataset(dataset, config)
+
+print(df)
 
 # start AL experiment
 
--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,17 @@
+from __future__ import annotations
+
 from enum import unique
 from enum import IntEnum
+from pathlib import Path
 from aenum import extend_enum
 from typing import Any, Dict, Tuple
 import pandas as pd
 import yaml
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from misc.config import Config
 
 
 @unique
@@ -17,3 +25,9 @@ with open("datasets/kaggle_parameters.yaml", "r") as params_file:
 
 for dataset_name in datasets_yaml_parameter_dict:
     extend_enum(Dataset, dataset_name)
+
+
+def load_dataset(dataset: Dataset, config: Config) -> pd.DataFrame:
+    dataset_file = dataset.name + ".csv"
+    dataset_path: Path = config.DATASETS_PATH / dataset_file
+    return pd.read_csv(dataset_path)
