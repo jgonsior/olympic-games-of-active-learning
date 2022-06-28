c41fa556fd628e13f8bcc419d148e2122f1ef99d
Julius Gonsior <julius@gaenseri.ch>
2022-06-28 18:35:57 +0200

    minor fixes

--- slurm_templates/chain_job.sh
+++ slurm_templates/chain_job.sh
@@ -4,4 +4,4 @@ export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
 workload_run=$(sbatch --parsable {{ HPC_OUTPUT_PATH}}/{{ EXP_TITLE }}/02_slurm.slurm)
-tar_file=$(sbatch --parsable --dependency=afterok:$workload_run {{ HPC_OUTPUT_PATH}}/{{ EXP_TITLE }}/02b_tar.slurm)
+tar_file=$(sbatch --parsable --dependency=afterok:$workload_run {{ HPC_OUTPUT_PATH}}/{{ EXP_TITLE }}/03_tar.slurm)
