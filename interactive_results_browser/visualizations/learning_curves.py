9785afc1ea66423f2819e27ef5d74f09d517669d
Julius Gonsior <julius@gaenseri.ch>
2022-11-03 21:07:36 +0100

    tadah, learning curves

--- interactive_results_browser/visualizations/learning_curves.py
+++ interactive_results_browser/visualizations/learning_curves.py
@@ -17,6 +17,7 @@ if TYPE_CHECKING:
     from misc.config import Config
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 
 class Learning_Curves(Base_Visualizer):
@@ -24,15 +25,19 @@ class Learning_Curves(Base_Visualizer):
     def get_additional_request_params() -> Dict[str, List[Any]]:
         return {
             "VIZ_LC_METRIC": [
-                "learner_training_time",
-                "query_selection_time",
-                "acc_auc",
-                "macro_f1_auc",
-                "macro_prec_auc",
-                "macro_recall_auc",
-                "weighted_f1_auc",
-                "weighted_prec_auc",
-                "weighted_recall_auc",
+                "accuracy",
+                # "precision", TODO <- die metriken gibt es je einmal pro klasse -> wie darstellen?
+                # "f1-score",
+                # "support",
+                # "recall",
+                "macro avg_precision",
+                "macro avg_recall",
+                "macro avg_f1-score",
+                "macro avg_support",
+                "weighted avg_precision",
+                "weighted avg_recall",
+                "weighted avg_f1-score",
+                "weighted avg_support",
             ]
         }
 
@@ -57,23 +62,41 @@ class Learning_Curves(Base_Visualizer):
         return df
 
     def get_template_data(self) -> Dict[str, Any]:
-        # read in all metrics
-        done_workload_df = self._load_done_workload()
-
-        # read in each csv file to get learning curve data for plot
-        for _, row in done_workload_df.iterrows():
-            print(row)
-
         if len(self._exp_grid_request_params["VIZ_LC_METRIC"]) != 1:
             return {"ERROR": "Please select only one VIZ_LC_METRIC value"}
 
-        fig = sns.relplot(
-            df,
-            x="EXP_STRATEGY",
-            y=self._exp_grid_request_params["VIZ_LC_METRIC"][0],
-            col="EXP_DATASET",
-            col_wrap=6,
+        metric = self._exp_grid_request_params["VIZ_LC_METRIC"][0]
+        # read in all metrics
+        done_workload_df = self._load_done_workload()
+
+        result_data = []
+        # read in each csv file to get learning curve data for plot
+        for _, row in done_workload_df.iterrows():
+            detailed_metrics_df = pd.read_csv(
+                f"{self._config.OUTPUT_PATH}/{row['EXP_DATASET']}/{row['EXP_UNIQUE_ID']}{self._config.METRIC_RESULTS_PATH_APPENDIX}",
+                usecols=[metric],
+            )
+
+            for ix, row2 in detailed_metrics_df.iterrows():
+                result_data.append(
+                    (row["EXP_STRATEGY"], row["EXP_DATASET"], str(ix), row2[metric])
+                )
+
+        results = pd.DataFrame(
+            data=result_data, columns=["Strategy", "Dataset", "#Iteration", metric]
         )
+        rel = sns.relplot(
+            results,
+            x="#Iteration",
+            y=metric,
+            hue="Strategy",
+            kind="line",
+            style="Strategy",
+            col="Dataset",
+            col_wrap=min(6, len(self._exp_grid_request_params["EXP_DATASET"])),
+        )
+        for ax in rel.fig.axes:
+            ax.xaxis.set_major_formatter(FuncFormatter(lambda x, _: int(x)))
 
         img = io.BytesIO()
         plt.savefig(img, format="png")
