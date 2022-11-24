0594a0f52a7a113dabca9218c1cd6b96ac9bf3a4
Julius Gonsior <julius@gaenseri.ch>
2022-11-24 13:19:26 +0100

    ignore time consuming large strategies

--- 02_run_experiment.py
+++ 02_run_experiment.py
@@ -4,7 +4,7 @@ from framework_runners.optimal_runner import OPTIMAL_AL_Experiment
 from framework_runners.libact_runner import LIBACT_Experiment
 from framework_runners.playground_runner import PLAYGROUND_AL_Experiment
 from misc.config import Config
-from resources.data_types import AL_FRAMEWORK
+from resources.data_types import AL_FRAMEWORK, AL_STRATEGY
 
 
 config = Config()
@@ -23,5 +23,21 @@ else:
     raise ValueError(
         "Error, could not find the specified AL Strategy or the framework does not exist"
     )
+
+
+al_strategies_to_temporarily_ignore = [
+    AL_STRATEGY.ALIPY_BMDR,
+    AL_STRATEGY.ALIPY_CORESET_GREEDY,
+    AL_STRATEGY.OPTIMAL_GREEDY,
+    AL_STRATEGY.ALIPY_LAL,
+    AL_STRATEGY.ALIPY_SPAL,
+    AL_STRATEGY.ALIPY_UNCERTAINTY_DTB,
+    AL_STRATEGY.ALIPY_QBC,
+    AL_STRATEGY.ALIPY_EXPECTED_ERROR_REDUCTION,
+]
+
+if config.EXP_STRATEGY in al_strategies_to_temporarily_ignore:
+    exit(-1)
+
 
 al_experiment.run_experiment()
