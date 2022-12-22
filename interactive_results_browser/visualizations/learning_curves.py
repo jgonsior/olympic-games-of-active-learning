263282ff64566eb845aef02b10de870b83b9cb20
Julius Gonsior <julius@gaenseri.ch>
2022-12-22 16:30:57 +0100

    leaning curves work with new data format

--- interactive_results_browser/visualizations/learning_curves.py
+++ interactive_results_browser/visualizations/learning_curves.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from pathlib import Path
 
 import pandas as pd
 
@@ -18,16 +19,17 @@ from interactive_results_browser.cache import memory
 
 def _plot_function(plot_df, metric, my_palette, my_markers):
     fig, ax = plt.subplots(figsize=(8, 4))
+
     rel = sns.lineplot(
         plot_df,
         ax=ax,
         x="AL Cycle",
         y=metric,
-        style="Strategy",
+        style="EXP_STRATEGY",
         # markers="Strategy",
         markers=my_markers,
         palette=my_palette,
-        hue="Strategy",
+        hue="EXP_STRATEGY",
     )
     rel.xaxis.set_major_formatter(FuncFormatter(lambda x, _: int(x)))
     rel.set(ylim=(0, 1))
@@ -36,34 +38,42 @@ def _plot_function(plot_df, metric, my_palette, my_markers):
 
 @memory.cache()
 def _cache_create_plots(
-    done_workload_df,
-    metric,
-    OUTPUT_PATH,
-    METRIC_RESULTS_PATH_APPENDIX,
+    done_workload_df: pd.DataFrame,
+    metric: str,
+    OUTPUT_PATH: Path,
 ) -> List[str]:
-    result_data = []
-    # read in each csv file to get learning curve data for plot
-    for _, row in done_workload_df.iterrows():
-        detailed_metrics_df = pd.read_csv(
-            f"{OUTPUT_PATH}/{row['EXP_DATASET']}/{row['EXP_UNIQUE_ID']}{METRIC_RESULTS_PATH_APPENDIX}",
-            usecols=[metric],
-        )
+    done_workload_df = done_workload_df.loc[
+        :, ["EXP_UNIQUE_ID", "EXP_STRATEGY", "EXP_DATASET"]
+    ]
 
-        for ix, row2 in detailed_metrics_df.iterrows():
-            result_data.append(
-                (row["EXP_STRATEGY"], row["EXP_DATASET"], str(ix), row2[metric])
+    plot_df = pd.DataFrame()
+
+    for EXP_STRATEGY in done_workload_df["EXP_STRATEGY"].unique():
+        for EXP_DATASET in done_workload_df["EXP_DATASET"].unique():
+            detailed_metrics_path = Path(
+                f"{OUTPUT_PATH}/{EXP_STRATEGY}/{EXP_DATASET}/{metric}.csv.gz"
             )
+            if detailed_metrics_path.exists():
+                # read in each csv file to get learning curve data for plot
+                detailed_metrics_df = pd.read_csv(detailed_metrics_path)
 
-    plot_df = pd.DataFrame(
-        data=result_data, columns=["Strategy", "Dataset", "AL Cycle", metric]
+                detailed_metrics_df = detailed_metrics_df.merge(
+                    done_workload_df, on="EXP_UNIQUE_ID", how="inner"
+                )
+                plot_df = pd.concat([plot_df, detailed_metrics_df], ignore_index=True)
+
+    del plot_df["EXP_UNIQUE_ID"]
+
+    plot_df = plot_df.melt(
+        id_vars=["EXP_STRATEGY", "EXP_DATASET"], var_name="AL Cycle", value_name=metric
     )
 
     plot_urls = Base_Visualizer._render_images(
         plot_df=plot_df,
         args={"metric": metric},
         plot_function=_plot_function,
-        df_col_key="Dataset",
-        legend_names=plot_df["Strategy"].unique(),
+        df_col_key="EXP_DATASET",
+        legend_names=plot_df["EXP_STRATEGY"].unique(),
     )
     return plot_urls
 
@@ -101,7 +111,6 @@ class Learning_Curves(Base_Visualizer):
             metric=metric,
             done_workload_df=done_workload_df,
             OUTPUT_PATH=self._config.OUTPUT_PATH,
-            METRIC_RESULTS_PATH_APPENDIX=self._config.METRIC_RESULTS_PATH_APPENDIX,
         )
 
         return {"plot_data": plot_url}
