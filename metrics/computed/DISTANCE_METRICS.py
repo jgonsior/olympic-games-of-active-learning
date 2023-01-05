47e74257d9462ab5c7199629cce80a9811ba26a1
Julius Gonsior <julius@gaenseri.ch>
2023-01-05 15:43:06 +0100

    vizualized

--- metrics/computed/DISTANCE_METRICS.py
+++ metrics/computed/DISTANCE_METRICS.py
@@ -34,7 +34,22 @@ class DISTANCE_METRICS(Base_Computed_Metric):
         df = df.apply(lambda x: [ast.literal_eval(iii) for iii in x], axis=0)
         return df
 
-    def apply_to_row(
+    def avg_dist_batch(
+        self, row: pd.Series, EXP_STRATEGY: AL_STRATEGY, EXP_DATASET: DATASET
+    ) -> pd.Series:
+        results = 0
+        for _, x in row.items():
+            distances = []
+            for s1, s2 in itertools.combinations(x, 2):
+                distances.append(self._precomputed_distances[s1][s2])
+
+            if len(distances) == 0:
+                results += 0
+            else:
+                results += sum(distances) / len(distances)
+        return results
+
+    def avg_dist_labeled
         self, row: pd.Series, EXP_STRATEGY: AL_STRATEGY, EXP_DATASET: DATASET
     ) -> pd.Series:
         results = 0
@@ -51,5 +66,12 @@ class DISTANCE_METRICS(Base_Computed_Metric):
 
     def compute(self) -> None:
         self._take_single_metric_and_compute_new_one(
-            existing_metric_name="selected_indices", new_metric_name="avg_dist_batch"
+            existing_metric_name="selected_indices",
+            new_metric_name="avg_dist_batch",
+            apply_to_row=self.avg_dist_batch,
+        )
+        self._take_single_metric_and_compute_new_one(
+            existing_metric_name="selected_indices",
+            new_metric_name="avg_dist_labeled",
+            apply_to_row=self.avg_dist_labeled,
         )
