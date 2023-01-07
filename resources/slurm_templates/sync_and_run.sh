2bf269c01dc86a3cf6d32a3cc9f7142e013c0fe7
Julius Gonsior <julius@gaenseri.ch>
2023-01-07 17:22:24 +0100

    fixed some stuff -> xz

--- resources/slurm_templates/sync_and_run.sh
+++ resources/slurm_templates/sync_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -avz -P {{ LOCAL_CODE_PATH }}/ {{ HPC_SSH_LOGIN }}:{{ HPC_CODE_PATH }} --exclude '.git/' --exclude '.mypy_cache/'
+rsync -avz -P {{ LOCAL_CODE_PATH }}/ {{ HPC_SSH_LOGIN }}:{{ HPC_CODE_PATH }} --exclude '.git/' --exclude '.mypy_cache/' --exclude '*.pyc' --exclude '__pycache__'
 rsync -avz -P {{ LOCAL_OUTPUT_PATH }}/ {{ HPC_SSH_LOGIN }}:{{ HPC_OUTPUT_PATH }}
 ssh {{ HPC_SSH_LOGIN }} << EOF
     cd {{ HPC_CODE_PATH }}
