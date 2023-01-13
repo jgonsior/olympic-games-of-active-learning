d09a7e6e44c8b6fbf28c2fc05790706ba074d26b
Julius Gonsior <julius@gaenseri.ch>
2023-01-13 14:27:30 +0100

    fix macro f1 score duplicates

--- scripts/fix_early_stopping_dict_keys_too_small_error.py
+++ scripts/fix_early_stopping_dict_keys_too_small_error.py
@@ -21,6 +21,13 @@ for file_name in glob.glob(str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True
     if metric_file.name.endswith("_workload.csv"):
         continue
 
+    def _fix_nan_rows(row):
+        if row.isnull().values.any():
+            no_null_row = row[~row.isnull()]
+            row[len(no_null_row) - 1] = np.nan
+            row[["EXP_UNIQUE_ID"]] = int(no_null_row.iloc[-1])
+        return row
+
     try:
         df = pd.read_csv(metric_file, header=0, delimiter=",")
     except pd.errors.ParserError:
@@ -39,13 +46,11 @@ for file_name in glob.glob(str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True
             ],
         )
 
-        def _fix_nan_rows(row):
-            if row.isnull().values.any():
-                no_null_row = row[~row.isnull()]
-                row[len(no_null_row) - 1] = np.nan
-                row[["EXP_UNIQUE_ID"]] = int(no_null_row.iloc[-1])
-            return row
+        df = df.apply(_fix_nan_rows, axis=1)
+        df["EXP_UNIQUE_ID"] = df["EXP_UNIQUE_ID"].astype(int)
+        df.to_csv(metric_file, index=False)
 
+    if df["EXP_UNIQUE_ID"].isnull().values.any():
         df = df.apply(_fix_nan_rows, axis=1)
         df["EXP_UNIQUE_ID"] = df["EXP_UNIQUE_ID"].astype(int)
         df.to_csv(metric_file, index=False)
