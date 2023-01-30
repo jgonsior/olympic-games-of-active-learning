aeab77ebc744635c677eb90d75e6df0ef3a76994
Julius Gonsior <julius@gaenseri.ch>
2023-01-30 15:24:08 +0100

    partial run workload results

--- 05_analyze_partially_run_workload.py
+++ 05_analyze_partially_run_workload.py
@@ -16,15 +16,25 @@ config = Config()
 
 
 df = pd.read_csv(config.OVERALL_DONE_WORKLOAD_PATH)
-
-df = (
-    df.groupby(["EXP_DATASET", "EXP_STRATEGY"])
-    .size()
-    .reset_index()
-    .rename(columns={0: "count"})
-    .sort_values("count")
-)
 df["EXP_DATASET"] = df["EXP_DATASET"].apply(lambda x: DATASET(int(x)).name)
 df["EXP_STRATEGY"] = df["EXP_STRATEGY"].apply(lambda x: AL_STRATEGY(int(x)).name)
-print(df)
-df.to_csv("test.csv", index=False)
+
+groupings = [
+    ["EXP_DATASET", "EXP_STRATEGY"],
+    ["EXP_DATASET"],
+    ["EXP_STRATEGY"],
+    # ["EXP_LEARNER_MODEL"],
+    # ["EXP_BATCH_SIZE"],
+    # ["EXP_START_POINT"],
+]
+
+
+for grouping in groupings:
+    df2 = (
+        df.groupby(grouping)
+        .size()
+        .reset_index()
+        .rename(columns={0: "count"})
+        .sort_values("count")
+    )
+    print(df2)
