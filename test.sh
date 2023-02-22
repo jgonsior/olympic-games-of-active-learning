62b903ea0e2ac6dc24571214c597ec64da19f95f
Julius Gonsior <julius@gaenseri.ch>
2023-02-22 12:42:21 +0100

    runtime

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-#python -m scripts.fix_duplicate_header_columns --EXP_TITLE full_exp --LOCAL_OUTPUT_PATH /home/mg/exp_results/full_exp.taurus
-python -m scripts.fix_early_stopping_dict_keys_too_small_error --EXP_TITLE full_exp --LOCAL_OUTPUT_PATH /home/mg/exp_results/full_exp.taurus
-python -m scripts.fix_macro_f1_score_duplicates --EXP_TITLE full_exp --LOCAL_OUTPUT_PATH /home/mg/exp_results/full_exp.taurus
-python -m scripts.fix_apply_runtime_limit_post_mortem --EXP_TITLE full_exp --LOCAL_OUTPUT_PATH /home/mg/exp_results/full_exp.taurus
+python -m scripts.fix_duplicate_header_columns --EXP_TITLE full_exp 
+python -m scripts.fix_early_stopping_dict_keys_too_small_error --EXP_TITLE full_exp 
+python -m scripts.fix_macro_f1_score_duplicates --EXP_TITLE full_exp 
+python -m scripts.fix_apply_runtime_limit_post_mortem --EXP_TITLE full_exp
 
+chmod u+x /home/mg/exp_results/full_exp/02c_gzip_results.sh.slurm
+
+cd /home/mg/exp_results/full_exp
+bash /home/mg/exp_results/full_exp/02c_gzip_results.sh.slurm
+
+cd /home/jg/al_olympics/code
+python 04_calculate_all_compound_metrics.py --EXP_TITLE full_exp --COMPUTED_METRICS DISTANCE_METRICS MISMATCH_TRAIN_TEST CLASS_DISTRIBUTIONS METRIC_DROP HARDEST_SAMPLES QUERIED_FROM_OPTIMAL    
