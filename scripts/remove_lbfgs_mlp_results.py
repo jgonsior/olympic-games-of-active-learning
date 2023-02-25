1367faf41d3f9822c0865ddf1e90cd5b437d744c
Julius Gonsior <julius@gaenseri.ch>
2023-02-25 16:46:03 +0100

    fixed stopit

--- scripts/remove_lbfgs_mlp_results.py
+++ scripts/remove_lbfgs_mlp_results.py
@@ -18,11 +18,22 @@ lbfgs_mlp_exp_ids = []
 
 
 df = pd.read_csv(config.OVERALL_DONE_WORKLOAD_PATH)
-lbfgs_mlp_exp_ids = df[df["EXP_LEARNER_MODEL"] == LEARNER_MODEL.LBFGS_MLP.value][
-    "EXP_UNIQUE_ID"
-]
+lbfgs_mlp_exp_ids = df[
+    df["EXP_LEARNER_MODEL"].isin(
+        [LEARNER_MODEL.MLP.value, LEARNER_MODEL.LBFGS_MLP.value]
+    )
+]["EXP_UNIQUE_ID"]
 
 for file_name in glob.glob(str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True):
+    print(file_name)
+    df = pd.read_csv(file_name)
+    a = len(df)
+    df = df[~df["EXP_UNIQUE_ID"].isin(lbfgs_mlp_exp_ids)]
+    if len(df) < a:
+        df.to_csv(file_name, index=False)
+
+
+for file_name in glob.glob(str(config.OUTPUT_PATH) + "/*.csv", recursive=True):
     print(file_name)
     df = pd.read_csv(file_name)
     a = len(df)
