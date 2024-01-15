3a78a572a8a28009a6de91f65f5276f464b9e979
Julius Gonsior <julius@gaenseri.ch>
2024-01-15 15:27:58 +0100

    old code

--- scripts/fix_check_if_dupicate_param_combinations_exist.py
+++ scripts/fix_check_if_dupicate_param_combinations_exist.py
@@ -3,6 +3,7 @@ import glob
 import sys
 
 import pandas as pd
+from sklearn.conftest import fetch_olivetti_faces
 
 
 sys.dont_write_bytecode = True
@@ -25,6 +26,7 @@ done_workload = pd.read_csv(config.OVERALL_DONE_WORKLOAD_PATH)
 failed_workload = pd.read_csv(config.OVERALL_FAILED_WORKLOAD_PATH)
 started_oom_workloads = pd.read_csv(config.OVERALL_STARTED_OOM_WORKLOAD_PATH)
 
+
 failed_workload.drop(columns="error", inplace=True)
 
 combined_df = pd.concat([done_workload, failed_workload, started_oom_workloads])
@@ -34,10 +36,12 @@ duplicates = combined_df.duplicated()
 
 duplicate_exp_unique_ids = combined_df[duplicates == True]["EXP_UNIQUE_ID"]
 
+print(duplicate_exp_unique_ids)
+
 for file_name in glob.glob(
     str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True
 ) + glob.glob(str(config.OUTPUT_PATH) + "*.csv", recursive=True):
-    print(file_name)
+    # print(file_name)
     df = pd.read_csv(file_name)
     a = len(df)
     df = df[~df["EXP_UNIQUE_ID"].isin(duplicate_exp_unique_ids)]
