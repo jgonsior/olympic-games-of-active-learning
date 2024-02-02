b15e9ec4791bfc640c61844db18d5ce2343879cd
Julius Gonsior <julius@gaenseri.ch>
2024-02-02 15:21:16 +0100

    dataset errors

--- scripts/remove_dataset_results.py
+++ scripts/remove_dataset_results.py
@@ -1,5 +1,7 @@
+import multiprocessing
 import sys
 import glob
+from joblib import Parallel, delayed
 
 import pandas as pd
 from datasets import DATASET
@@ -39,7 +41,8 @@ exp_ids_to_remove = []
 df = pd.read_csv(config.OVERALL_DONE_WORKLOAD_PATH)
 exp_ids_to_remove = df[df["EXP_DATASET"].isin(datasets_to_remove)]["EXP_UNIQUE_ID"]
 
-for file_name in glob.glob(str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True):
+
+def _do_stuff(file_name):
     print(file_name)
     df = pd.read_csv(file_name)
     a = len(df)
@@ -48,10 +51,21 @@ for file_name in glob.glob(str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True
         df.to_csv(file_name, index=False, compression="infer")
 
 
-for file_name in glob.glob(str(config.OUTPUT_PATH) + "/*.csv", recursive=True):
-    print(file_name)
-    df = pd.read_csv(file_name)
-    a = len(df)
-    df = df[~df["EXP_UNIQUE_ID"].isin(exp_ids_to_remove)]
-    if len(df) < a:
-        df.to_csv(file_name, index=False, compression="infer")
+glob_list = [
+    f for f in glob.glob(str(config.OUTPUT_PATH) + "/**/*.csv", recursive=True)
+]
+
+glob_list = set(
+    [
+        *glob_list,
+        *[f for f in glob.glob(str(config.OUTPUT_PATH) + "/*.csv", recursive=True)],
+    ]
+)
+print(len(glob_list))
+
+
+Parallel(n_jobs=multiprocessing.cpu_count(), verbose=10)(
+    # Parallel(n_jobs=1, verbose=10)(
+    delayed(_do_stuff)(file_name)
+    for file_name in glob_list
+)
