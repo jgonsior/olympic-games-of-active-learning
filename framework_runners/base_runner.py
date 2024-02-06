580f46bc2dfd39c9259c3c7222d6263061b98665
Julius Gonsior <julius@gaenseri.ch>
2024-02-06 12:27:01 +0100

    dataset errors

--- framework_runners/base_runner.py
+++ framework_runners/base_runner.py
@@ -159,8 +159,7 @@ class AL_Experiment(ABC):
         early_stopped_due_to_runtime_limit = False
         error_was_being_raised = False
 
-        class NonExistentDummyException(Exception):
-            ...
+        class NonExistentDummyException(Exception): ...
 
         try:
             for iteration in range(0, total_amount_of_iterations):
@@ -178,7 +177,7 @@ class AL_Experiment(ABC):
 
                     early_stopped_due_to_runtime_limit = True
                     break
-        except Exception as err:
+        except NonExistentDummyException as err:
             error_was_being_raised = True
             import sys
 
