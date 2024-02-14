014b3641ca2fa48b7130eeaac83801d1714ea0f0
Julius Gonsior <julius@gaenseri.ch>
2024-02-14 13:56:26 +0100

    ramp slope thresholds

--- scripts/calculate_dataset_dependend_random_ramp_slope.py
+++ scripts/calculate_dataset_dependend_random_ramp_slope.py
@@ -1,14 +1,42 @@
 import csv
+from math import e
 from pathlib import Path
+from re import I
 import sys
 import glob
+from typing import Dict, Tuple
 
 from joblib import Parallel, delayed
+from matplotlib import pyplot as plt
 import pandas as pd
+import seaborn as sns
+from datasets import DATASET
+
 
 sys.dont_write_bytecode = True
 
 from misc.config import Config
+
+font_size = 5.8
+tex_fonts = {
+    # Use LaTeX to write all text
+    "text.usetex": False,
+    # "text.usetex": False,
+    # "font.family": "times",
+    # Use 10pt font in plots, to match 10pt font in document
+    "axes.labelsize": font_size,
+    "font.size": font_size,
+    # Make the legend/label fonts a little smaller
+    "legend.fontsize": font_size,
+    "xtick.labelsize": font_size,
+    "ytick.labelsize": font_size,
+    "xtick.bottom": True,
+    # "figure.autolayout": True,
+}
+
+plt.rcParams.update(tex_fonts)  # type: ignore
+
+sns.set_theme(rc={"figure.figsize": (12, 6)}, style="white", context="paper")
 
 config = Config()
 
@@ -28,31 +56,53 @@ if not ramp_plateau_results_file.exists():
 # check all buckets of 5 AL cycles for stationary
 # when change from "stationary" to "non-stationary" --> we have a slope!
 
-# EXP_DATASET
-# EXP_BATCH_SIZE
-# EXP_LEARNER_MODEL
-# EXP_TRAIN_TEST_BUCKET_SIZE
+# (EXP_DATASET, EXP_BATCH_SIZE, EXP_LEARNER_MODEL, EXP_TRAIN_TEST_BUCKET_SIZE):17
+cutoff_values: Dict[Tuple[str, int, int, int], float] = {}
 
 
 def _do_stuff(file_name, config):
     metric_file = Path(file_name)
     print(metric_file)
     df = pd.read_csv(metric_file)
-    print(df)
+    # print(df)
 
-    # merge those rows together which belong together based on
+    small_done_df = done_workload_df.loc[
+        done_workload_df["EXP_UNIQUE_ID"].isin(df.EXP_UNIQUE_ID)
+    ]
+    EXP_DATASET = DATASET(small_done_df["EXP_DATASET"].iloc[0])
 
-    # EXP_DATASET
-    # EXP_BATCH_SIZE
-    # EXP_LEARNER_MODEL
-    # EXP_TRAIN_TEST_BUCKET_SIZE
+    del small_done_df["EXP_STRATEGY"]
+    del small_done_df["EXP_DATASET"]
+    del small_done_df["EXP_RANDOM_SEED"]
+    del small_done_df["EXP_NUM_QUERIES"]
 
-    exit(-1)
+    grouped = small_done_df.groupby(
+        by=["EXP_BATCH_SIZE", "EXP_LEARNER_MODEL", "EXP_TRAIN_TEST_BUCKET_SIZE"]
+    )["EXP_UNIQUE_ID"].apply(list)
+    # print(grouped)
+
+    for k, v in grouped.items():
+        # print(k)
+
+        interesting_rows = df.loc[df["EXP_UNIQUE_ID"].isin(v)]
+        del interesting_rows["EXP_UNIQUE_ID"]
+        # print(interesting_rows)
+        melted_data = interesting_rows.melt(var_name="x", value_name="val")
+        ax = sns.lineplot(data=melted_data, x="x", y="val")
+
+        ax.set_title(f"{EXP_DATASET.name} - {k}")
+        plt.savefig(
+            f"plots_cutoffs/{EXP_DATASET.name}-{k}.jpg",
+            dpi=300,
+            bbox_inches="tight",
+            pad_inches=0,
+        )
+        plt.clf()
 
 
 for EXP_DATASET in config.EXP_GRID_DATASET:
-    if EXP_DATASET.name in ["Iris", "wine_origin"]:
-        continue
+    # if EXP_DATASET.name in ["Iris", "wine_origin"]:
+    #    continue
     glob_list = [
         f
         for f in glob.glob(
