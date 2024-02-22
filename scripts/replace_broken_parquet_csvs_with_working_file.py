c90f7d32bcf8cc195d907219596ff6360f32e7eb
Julius Gonsior <julius@gaenseri.ch>
2024-02-22 23:48:31 +0100

    fixed dataset dependend random ramp slope saving

---
+++ scripts/replace_broken_parquet_csvs_with_working_file.py
@@ -0,0 +1,33 @@
+import sys
+import glob
+import pandas as pd
+
+from misc.helpers import _get_df, _get_glob_list
+
+sys.dont_write_bytecode = True
+
+from misc.config import Config
+from pandarallel import pandarallel
+from joblib import Parallel, delayed
+import multiprocessing
+from pathlib import Path
+
+# all batches which have been running longer than 10 minutes will be ignored
+
+pandarallel.initialize(progress_bar=True)
+config = Config()
+
+
+broken_csv_files = pd.read_csv(config.BROKEN_CSV_FILE_PATH)
+
+for broken_csv_file in broken_csv_files["metric_file"]:
+    broken_csv_file = Path(broken_csv_file)
+    broken_csv_file.unlink()
+
+    original_y_pred = Path(
+        "/home/thiele/exp_results/bkp_01_full_exp_jan/"
+        + broken_csv_file.name.removesuffix(".parquet")
+    )
+
+    df = pd.read_csv(original_y_pred)
+    df.to_parquet(broken_csv_file)
