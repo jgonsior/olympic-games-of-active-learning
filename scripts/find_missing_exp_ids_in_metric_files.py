9bfd204d444950993d8ecd1447b8f20400837c03
Julius Gonsior <julius@gaenseri.ch>
2024-02-23 13:11:51 +0100

    fixed dataset dependend random ramp slope saving

--- scripts/find_missing_exp_ids_in_metric_files.py
+++ scripts/find_missing_exp_ids_in_metric_files.py
@@ -39,15 +39,15 @@ def _is_standard_metric(metric_path: Path) -> bool:
     ]
 
     for sm in standard_metrics:
-        if f"/{sm}.csv" in metric_path.name:
+        if f"{sm}.csv.xz" == metric_path.name:
             return True
+        elif f"{sm}.csv.xz.parquet" == metric_path.name:
+            return True
+
     return False
 
 
 def _do_stuff(exp_dataset, exp_strategy, config):
-    if exp_strategy.name != "SKACTIVEML_DWUS":
-        return
-
     glob_list = _get_glob_list(config, limit=f"/{exp_strategy.name}/{exp_dataset.name}")
 
     if len(glob_list) == 0:
@@ -60,8 +60,11 @@ def _do_stuff(exp_dataset, exp_strategy, config):
     for file_name in glob_list:
         if not _is_standard_metric(file_name):
             print(f"Not standard metric: {file_name}")
-        metric_name = Path(file_name).name.removesuffix(".csv.xz")
-        metric_name = Path(file_name).name.removesuffix(".csv.xz.parquet")
+
+        if file_name.name.endswith(".parquet"):
+            metric_name = file_name.name.removesuffix(".csv.xz.parquet")
+        else:
+            metric_name = file_name.name.removesuffix(".csv.xz")
 
         metric_df = _get_df(file_name, config)
 
@@ -75,23 +78,29 @@ def _do_stuff(exp_dataset, exp_strategy, config):
         )
 
         exp_ids_union = exp_ids_union.union(exp_ids_per_metric[metric_name])
+
     exp_ids_intersection = copy.deepcopy(exp_ids_union)
 
-    for metric, exp_ids in exp_ids_per_metric.items():
+    for _, exp_ids in exp_ids_per_metric.items():
         exp_ids_intersection = exp_ids_intersection.intersection(exp_ids)
 
-    if len(exp_ids_intersection) < len(exp_ids_union):
-        for broken_exp_id in exp_ids_intersection:
-            _append_and_create(
-                config.MISSING_EXP_IDS_IN_METRIC_FILES,
-                done_workload_df.loc[
-                    done_workload_df["EXP_UNIQUE_ID"] == broken_exp_id
-                ].to_dict(),
-            )
+    difference = exp_ids_union.symmetric_difference(exp_ids_intersection)
+    if len(difference) > 0:
+        for broken_exp_id in difference:
+            potential_rows_to_be_deleted = done_workload_df.loc[
+                done_workload_df["EXP_UNIQUE_ID"] == broken_exp_id
+            ]
+
+            if len(potential_rows_to_be_deleted) > 0:
+                row_to_be_deleted = potential_rows_to_be_deleted.iloc[0]
+                _append_and_create(
+                    config.MISSING_EXP_IDS_IN_METRIC_FILES,
+                    row_to_be_deleted.to_dict(),
+                )
 
 
-Parallel(n_jobs=1, verbose=10)(
-    # Parallel(n_jobs=multiprocessing.cpu_count(), verbose=10)(
+# Parallel(n_jobs=1, verbose=10)(
+Parallel(n_jobs=multiprocessing.cpu_count(), verbose=10)(
     delayed(_do_stuff)(exp_dataset, exp_strategy, config)
     for (exp_dataset, exp_strategy) in itertools.product(
         config.EXP_GRID_DATASET, config.EXP_GRID_STRATEGY
