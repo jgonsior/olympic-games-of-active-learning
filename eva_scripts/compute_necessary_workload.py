8362b262657ec9ceff25bb21874b9b249d54d7bd
Julius Gonsior <julius@gaenseri.ch>
2024-02-25 18:52:23 +0100

    compute necesserary workload

--- eva_scripts/compute_necessary_workload.py
+++ eva_scripts/compute_necessary_workload.py
@@ -1,29 +1,33 @@
-from collections import defaultdict
+import ast
+import copy
+import csv
 import itertools
 from pathlib import Path
 import sys
 import glob
-import warnings
+from joblib import Parallel, delayed
 from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
-from tqdm import tqdm
-from datasets import DATASET
+import multiprocessing
+import seaborn as sns
+from sklearn.isotonic import spearmanr
+from sklearn.metrics import jaccard_score
 
-from resources.data_types import AL_STRATEGY
+from misc.helpers import _append_and_create, _get_df, _get_glob_list
+from misc.plotting import set_matplotlib_size, set_seaborn_style
 
 sys.dont_write_bytecode = True
 
 from misc.config import Config
-import numpy as np
-import seaborn as sns
-
-from scipy.stats import spearmanr
-
 
 config = Config()
 
 
-done_workload = pd.read_csv(config.OVERALL_DONE_WORKLOAD_PATH)
+done_workload_df = pd.read_csv(config.OVERALL_DONE_WORKLOAD_PATH)
+
+del done_workload_df["EXP_RANDOM_SEED"]
+del done_workload_df["EXP_NUM_QUERIES"]
 
 column_combinations = [
     "EXP_DATASET",
@@ -37,137 +41,85 @@ column_combinations = [
 ]
 
 
-def _calculate_correlations(param_to_evaluate):
-    dense_workload = pd.read_csv(
-        config.DENSE_WORKLOAD_PATH,
-    )
+print(f"Original: {len(done_workload_df)}")
 
-    print(f"Original: {len(dense_workload)}")
-    print(param_to_evaluate)
+# each row contains fingerprint -> I want to reduce this whole thing to a correlation among these fingerprints
+# I calculate for each fingerprint, how good the individual strategies are -> I save the ranking values
+# in the end I get a pd.DataFrame(colums=["fingerprint", "strat_a", "strat_b", "strat_c", …])
+# and in each strat_a, strat_b, strat_c column I have the single_metric result for this strategy
+# then I calculate the correlation between the time series of strategy results
+# claudio frage: macht es Sinn darüber zu entscheiden, welche hyperparameter combinations ich verwenden soll?
+"""dense_workload_grouped = done_workload_df.groupby(
+    # by=[ddd for ddd in column_combinations if ddd != param_to_evaluate]
+    by=column_combinations
+).apply(lambda r: list(zip(r["EXP_STRATEGY"], r["EXP_UNIQUE_ID"])))
 
-    # each row contains fingerprint -> I want to reduce this whole thing to a correlation among these fingerprints
-    # I calculate for each fingerprint, how good the individual strategies are -> I save the ranking values
-    # in the end I get a pd.DataFrame(colums=["fingerprint", "strat_a", "strat_b", "strat_c", …])
-    # and in each strat_a, strat_b, strat_c column I have the single_metric result for this strategy
-    # then I calculate the correlation between the time series of strategy results
-    # claudio frage: macht es Sinn darüber zu entscheiden, welche hyperparameter combinations ich verwenden soll?
-    dense_workload_grouped = dense_workload.groupby(
-        # by=[ddd for ddd in column_combinations if ddd != param_to_evaluate]
-        by=column_combinations
-    ).apply(lambda r: list(zip(r["EXP_STRATEGY"], r["EXP_UNIQUE_ID"])))
-
-    print(
-        f"Calculating correlations for {param_to_evaluate}: {len(dense_workload_grouped)}"
-    )
-    print(dense_workload_grouped)
-
-    combined_stats = []
-
-    pbar = tqdm(
-        dense_workload_grouped.reset_index().iterrows(),
-        total=dense_workload_grouped.shape[0],
-    )
-    for _, row in pbar:
-
-        print(row)
-        exit(-1)
-        path_glob = f"{config.OUTPUT_PATH}/{AL_STRATEGY(row.EXP_STRATEGY).name}/{DATASET(row.EXP_DATASET).name}/*.csv.xz"
-
-        pbar.set_description(path_glob)
-
-        metrics_data = defaultdict(lambda: defaultdict(int))
-        for metric_path in glob.glob(
-            path_glob,
-            recursive=True,
-        ):
-            metrics_not_suitable_for_comparisons = [
-                "selected_indices",
-                "y_pred_test",
-                "y_pred_train",
-                "query_selection_time",
-                "learner_training_time",
-            ]
-
-            ignore_metric = False
-            for mnsfc in metrics_not_suitable_for_comparisons:
-                if metric_path.endswith(f"{mnsfc}.csv.xz"):
-                    ignore_metric = True
-                    continue
-            if ignore_metric:
-                continue
-
-            # ignore cut-point/auc metrics because they are simply summed/averaged over the other metrics -> the correlations will be found in the original metrics, not in them!
-            if "auc_" in metric_path:
-                continue
-            if "learning_stability_" in metric_path:
-                continue
-
-            metric_path = Path(metric_path)
-            # print(metric_path)
-
-            metric_df = pd.read_csv(metric_path)
-
-            if metric_df.shape[1] < 3:
-                print("Metric has too few columns, exiting.")
-                print(metric_path)
-                exit(-1)
-
-            for param_to_evaluate_value, EXP_UNIQUE_ID in row[0]:
-                # print(param_to_evaluate_value)
-                value = (
-                    metric_df.loc[metric_df["EXP_UNIQUE_ID"] == EXP_UNIQUE_ID]
-                    .drop("EXP_UNIQUE_ID", axis=1)
-                    .iloc[0]
-                    .to_list()
-                )
-                metric_name = str(metric_path.name).removesuffix(".csv.xz")
-
-                with warnings.catch_warnings():
-                    warnings.simplefilter("ignore", category=RuntimeWarning)
-                    value = [
-                        (
-                            np.nanmean(ast.literal_eval(str(vvv)))
-                            if str(vvv) != "nan"
-                            else np.nan
-                        )
-                        for vvv in value
-                    ]
-                metrics_data[param_to_evaluate_value][
-                    metric_name
-                ] = value  # np.nanmean(value)
-
-        # print(metrics_data)
-        metrics_df = pd.DataFrame(metrics_data)
-        # print(metrics_df)
-
-        # now i calculate the correlation PER metric pair!
-
-        metrics_df["spearmanr_stat"], metrics_df["spearmanr_pvalue"] = zip(
-            *metrics_df.apply(
-                lambda rrr: spearmanr(rrr.iloc[0], rrr.iloc[1], nan_policy="omit"),
-                axis=1,
-            )
-        )
-
-        # print(metrics_df)
-
-        combined_stats.append(
-            [metrics_df["spearmanr_stat"], metrics_df["spearmanr_pvalue"]]
-        )
-
-    print(combined_stats)
-    exit(-1)
+print(f"Calculating correlations for: {len(dense_workload_grouped)}")
+"""
 
 
-for cc in [
-    "EXP_BATCH_SIZE",
-    "EXP_DATASET",
-    "EXP_STRATEGY",
-    # "EXP_RANDOM_SEED",
-    "EXP_START_POINT",
-    # "EXP_NUM_QUERIES",
-    "EXP_LEARNER_MODEL",
-    "EXP_TRAIN_TEST_BUCKET_SIZE",
-    # "METRICS",
-]:
-    _calculate_correlations(cc)
+def _do_stuff(file_name: Path, config: Config, done_workload_df: pd.DataFrame):
+    print(file_name)
+
+    strategy_name = file_name.parent.parent.name
+
+    metric_df = _get_df(file_name, config)
+
+    if metric_df is None:
+        return
+
+    metric_df = pd.merge(metric_df, done_workload_df, on=["EXP_UNIQUE_ID"], how="left")
+
+    return metric_df
+
+
+glob_list = _get_glob_list(config, limit=f"**/full_auc_accuracy")
+
+# metric_dfs = Parallel(n_jobs=1, verbose=10)(
+metric_dfs = Parallel(n_jobs=multiprocessing.cpu_count(), verbose=10)(
+    delayed(_do_stuff)(file_name, config, done_workload_df) for file_name in glob_list
+)
+
+df = pd.concat(metric_dfs)
+del df["EXP_UNIQUE_ID"]
+
+non_al_cycle_keys = df.columns.difference(["0", "EXP_STRATEGY"])
+
+# replace non_al_cycle_keys by single string fingerprint as key
+df["fingerprint"] = df[non_al_cycle_keys].apply(
+    lambda row: "_".join(row.values.astype(str)), axis=1
+)
+
+for non_al_cycle_key in non_al_cycle_keys:
+    del df[non_al_cycle_key]
+
+print(df)
+df = df.pivot(index="fingerprint", columns="EXP_STRATEGY", values="0").reset_index()
+df.columns.name = None
+df = df.T
+
+df.columns = df.loc["fingerprint"].values
+df.drop(index="fingerprint", axis=0, inplace=True)
+
+df = df.corr()
+print(df)
+print(non_al_cycle_keys)
+
+result_folder = Path(config.OUTPUT_PATH / f"plots/")
+result_folder.mkdir(parents=True, exist_ok=True)
+
+
+df.to_parquet(result_folder / "fingerprint_correlations_based_on_single_metric.parquet")
+# summed_up_corr_values.loc[:, "Total"] = summed_up_corr_values.mean(axis=1)
+# summed_up_corr_values.sort_values(by=["Total"], inplace=True)
+
+set_seaborn_style(font_size=8)
+fig = plt.figure(figsize=set_matplotlib_size())
+sns.heatmap(df, annot=True)
+
+plt.savefig(
+    result_folder / "fingerprint_correlations_based_on_single_metric.jpg",
+    dpi=300,
+    bbox_inches="tight",
+    pad_inches=0,
+)
