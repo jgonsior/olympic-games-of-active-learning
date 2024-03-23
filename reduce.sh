32f0eccd12ee1a33a9e3f523ead33ec4ffcca709
Julius Gonsior <julius@gaenseri.ch>
2024-03-23 18:22:33 +0100

    reduce

--- reduce.sh
+++ reduce.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.3 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.3 --EVA_MODE local
@@ -8,7 +8,7 @@ done
 
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_030
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.4 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.4 --EVA_MODE local
@@ -16,7 +16,7 @@ do
 done
 
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_040
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.5 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.5 --EVA_MODE local
@@ -25,7 +25,7 @@ done
 
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_050
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.6 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.6 --EVA_MODE local
@@ -34,7 +34,7 @@ done
 
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_060
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.7 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.7 --EVA_MODE local
@@ -43,7 +43,7 @@ done
 
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_070
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.8 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.8 --EVA_MODE local
@@ -53,7 +53,7 @@ done
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_080
 
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.9 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.9 --EVA_MODE local
@@ -63,7 +63,7 @@ done
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_090
 
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.95 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.95 --EVA_MODE local
@@ -72,7 +72,7 @@ done
 
 mv /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction /home/thiele/exp_results/full_exp_jan/workloads/workload_reduction_095
 
-for i in $(seq 0 100);
+for i in $(seq 0 50);
 do
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.99 --EVA_MODE create --WORKER_INDEX $i
     python -m eva_scripts.workload_reduction --EXP_TITLE full_exp_jan --EVA_WORKLOAD_REDUCTION_THRESHOLD 0.99 --EVA_MODE local
